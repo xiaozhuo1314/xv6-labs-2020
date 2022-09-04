f1a7f01b6fe49744be82648bee68209de9fe17ed
xiaozhuo1314 <chenzhuo1116@qq.com>
2022-09-04 23:26:29 +0800

    switching between threads 完成

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,31 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+/*
+ * 由于其他函数在调用thread_switch的时候已经保存了caller需要保存的寄存器
+ * 所以这里只需要保存callee需要保存的寄存器即可
+ */
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+  uint64 s0; // fp
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct thread_context tcontext; // 线程上下文
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,6 +83,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)(&(current_thread->tcontext)), (uint64)(&(t->tcontext)));
   } else
     next_thread = 0;
 }
@@ -77,6 +98,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  // 线程刚创建时,只需要设定线程栈,且ra指向想要运行的函数即可
+  // 这样在切换后线程会在自己的栈中运行func
+  t->tcontext.ra = (uint64)func;
+  t->tcontext.sp = (uint64)(t->stack) + STACK_SIZE; // 由于栈是从高地址往低地址生长,所以这里需要加上STACK_SIZE
 }
 
 void 
