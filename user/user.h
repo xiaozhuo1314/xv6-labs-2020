6b839ff6f03fd8cea9ece5dad60cfd64966cb4a0
xiaozhuo1314 <chenzhuo1116@qq.com>
2022-09-19 00:15:09 +0800

    system link刚开始

--- user/user.h
+++ user/user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int symlink(char *, char *); // user add: 参数char *target, char *path
 
 // ulib.c
 int stat(const char*, struct stat*);
